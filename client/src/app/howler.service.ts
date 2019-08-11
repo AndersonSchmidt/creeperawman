75c7892bcaf5422af353f091a097e586f5f8e414
Anderson Schmidt <schmidtanderson@hotmail.com>
2019-08-11 14:17:23 -0300

    Finishing the song lyrics

--- client/src/app/howler.service.ts
+++ client/src/app/howler.service.ts
@@ -19,7 +19,20 @@ export class HowlerService {
     'right behind', 'right-right behind', 'that\'s a nice life you have', 'shame it\'s gotta end at this', 'time, time, time',
     'time-time-time-time', 'blows up', 'then your health bar drops and you could use a one-up', 'get inside, don\'t be tardy',
     'so now you\'re stuck in there', 'half a heart is left, but don\'t', 'die, die, die', 'die-die-die',
-    'until the sun comes up in the morn', 'cause, baby, tonight'
+    'until the sun comes up in the morn', 'cause, baby, tonight', 'the creeper\'s trying to steal all our stuff again (creepers)',
+    'you\'re mine, haha', 'dig up diamonds', 'and craft those diamonds', 'and make some armor',
+    'get it, baby, go and forge that like you so mlg pro', 'the sword\'s made of diamonds, so come at me bro huh',
+    'training in your room under the torchlight', 'hone that form to get you ready for the big fight',
+    'every single day and the whole night', 'creeper\'s out prowlin, woo, alright', 'look at me, look at you',
+    'take my revenge, that\'s what i\'m gonna do', 'i\'m a warrior, baby, what else is new',
+    'and my blade\'s gonna tear through you (cause baby tonight) bring it',
+    'the creeper\'s trying to steal all our stuff again (gather your stuff, yeah)', 'let\'s take back the world (yeah baby tonight) haha',
+    'grab your sword, armor and go (it\'s on)', 'take your revenge (woo)', 'oh-oh-oh-oh so fight, fight, like it\'s the last',
+    'last night of your life, life', 'show them your bite (woo)', 'cause, baby, tonight (ah ah)',
+    '(ah ah) the creeper\'s trying to steal all our stuff again',
+    'grab your pick, shovel and bolt again (bolt again-gain, woo)', 'and run run until it\'s done, done', 'cause baby, tonight',
+    '(come on, swing your sword up high) the creeper\'s trying to steal all our stuff again (come on, jab your sword down low)',
+    'woo', 'easter egg'
   ];
 
   constructor(private http: HttpClient) {
